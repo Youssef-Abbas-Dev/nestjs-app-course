21cb5b9f948adfc085a5b54e38906e0577474dc0
Youssef Abbas <youssefabbasawad@gmail.com>
2024-08-22 07:06:37 +0330

    Lesson 04

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,17 @@
-import { Controller, Get, Post, Body, Param, NotFoundException, Put, Delete } from "@nestjs/common";
+import {
+    Controller,
+    Get,
+    Post,
+    Body,
+    Param,
+    NotFoundException,
+    Put,
+    Delete,
+    Req,
+    Res,
+    Headers,
+} from "@nestjs/common";
+import { Request, Response } from 'express';
 import { CreateProductDto } from "./dtos/create-product.dto";
 import { UpdateProductDto } from "./dtos/update-product.dto";
 type ProductType = { id: number, title: string, price: number }
@@ -10,6 +23,30 @@ export class ProductsController {
         { id: 2, title: 'pen', price: 5 },
         { id: 3, title: 'laptop', price: 400 },
     ];
+
+    // // POST: ~/api/products/express-way
+    // @Post("express-way")
+    // public createNewProductExpressWay(
+    //     @Req() req: Request,
+    //     @Res({ passthrough: true }) res: Response,
+    //     @Headers() headers: any) {
+
+    //     const newProduct: ProductType = {
+    //         id: this.products.length + 1,
+    //         title: req.body.title,
+    //         price: req.body.price
+    //     }
+    //     this.products.push(newProduct);
+    //     console.log(headers);
+    //     console.log(req.headers);
+
+    //     res.status(201).json(newProduct);
+
+    //     // res.cookie('authCookie', 'this is a cookie', {
+    //     //     httpOnly: true,
+    //     //     maxAge: 120,
+    //     // })
+    // }
 
     // POST: ~/api/products
     @Post()
@@ -31,7 +68,7 @@ export class ProductsController {
 
     // GET: ~/api/products/:id
     @Get(":id")
-    public getSingleProducts(@Param("id") id: string) {
+    public getSingleProduct(@Param("id") id: string) {
         const product = this.products.find(p => p.id === parseInt(id));
         if (!product) throw new NotFoundException("product not found");
         return product;
