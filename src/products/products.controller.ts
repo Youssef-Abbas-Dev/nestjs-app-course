8e5df861d90e52b23ed18b62906dd0f8c68b999c
Youssef Abbas <youssefabbasawad@gmail.com>
2024-08-27 10:04:21 +0330

    Lesson 08

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -4,68 +4,48 @@ import {
     Post,
     Body,
     Param,
-    NotFoundException,
     Put,
     Delete,
     ParseIntPipe,
 } from "@nestjs/common";
 import { CreateProductDto } from "./dtos/create-product.dto";
 import { UpdateProductDto } from "./dtos/update-product.dto";
-type ProductType = { id: number, title: string, price: number }
+import { ProductsService } from "./products.service";
+
+
 
 @Controller("api/products")
 export class ProductsController {
-    private products: ProductType[] = [
-        { id: 1, title: 'book', price: 10 },
-        { id: 2, title: 'pen', price: 5 },
-        { id: 3, title: 'laptop', price: 400 },
-    ];
-
+    // @IMPORTANT NOTE: this considered as bad practice, we will fix it in the next lesson
+    private productsService: ProductsService = new ProductsService();
 
     // POST: ~/api/products
     @Post()
     public createNewProduct(@Body() body: CreateProductDto) {
-        console.log(body);
-        const newProduct: ProductType = {
-            id: this.products.length + 1,
-            title: body.title,
-            price: body.price
-        }
-        this.products.push(newProduct);
-        return newProduct;
+        return this.productsService.createProduct(body);
     }
 
     // GET: ~/api/products
     @Get()
     public getAllProducts() {
-        return this.products;
+        return this.productsService.getAll();
     }
 
     // GET: ~/api/products/:id
     @Get(":id")
     public getSingleProduct(@Param("id", ParseIntPipe) id: number) {
-        const product = this.products.find(p => p.id === id);
-        if (!product) throw new NotFoundException("product not found");
-        return product;
+        return this.productsService.getOneBy(id);
     }
 
     // PUT: ~/api/products/:id
     @Put(":id")
-    public updateProduct(
-        @Param('id', ParseIntPipe) id: string,
-        @Body() body: UpdateProductDto) {
-        const product = this.products.find(p => p.id === parseInt(id));
-        if (!product) throw new NotFoundException("product not found");
-
-        console.log(body);
-        return { message: 'product updated successfully with id ' + id };
+    public updateProduct(@Param('id', ParseIntPipe) id: string, @Body() body: UpdateProductDto) {
+        return this.productsService.update(id, body);
     }
 
     // DELETE: ~/api/products/:id
     @Delete(":id")
     public deleteProduct(@Param("id", ParseIntPipe) id: string) {
-        const product = this.products.find(p => p.id === parseInt(id));
-        if (!product) throw new NotFoundException("product not found");
-        return { message: 'product deleted' };
+      return this.productsService.delete(id);
     }
 }
