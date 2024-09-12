067d12e8a1525ea95a3a52db7f5e3fe054ac5e1d
Youssef Abbas <youssefabbasawad@gmail.com>
2024-09-12 16:20:24 +0330

    Lesson 18

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,21 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Post, UseGuards } from "@nestjs/common";
+import {
+    Body,
+    Controller,
+    Get,
+    HttpCode,
+    HttpStatus,
+    Post,
+    UseGuards
+} from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { RegisterDto } from "./dtos/register.dto";
 import { LoginDto } from "./dtos/login.dto";
 import { AuthGuard } from "./guards/auth.guard";
+import { AuthRolesGuard } from "./guards/auth-roles.guard";
 import { CurrentUser } from "./decorators/current-user.decorator";
 import { JWTPayloadType } from "src/utils/types";
+import { Roles } from "./decorators/user-role.decorator";
+import { UserType } from "../utils/enums"
 
 @Controller("api/users")
 export class UsersController {
@@ -30,5 +41,13 @@ export class UsersController {
     @UseGuards(AuthGuard)
     public getCurrentUser(@CurrentUser() payload: JWTPayloadType) {
         return this.usersService.getCurrentUser(payload.id);
+    }
+
+    // GET: ~/api/users
+    @Get()
+    @Roles(UserType.ADMIN)
+    @UseGuards(AuthRolesGuard)
+    public getAllUsers() {
+        return this.usersService.getAll();
     }
 }
