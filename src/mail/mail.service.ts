f62c16da2d3ab69b1c870353080e0be03ba8b74c
Youssef Abbas <youssefabbasawad@gmail.com>
2024-10-10 15:33:01 +0330

    Lesson 32

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -2,28 +2,23 @@ import { MailerService } from "@nestjs-modules/mailer";
 import { Injectable, RequestTimeoutException } from "@nestjs/common";
 
 @Injectable()
-export class MailService {
+export class MailService { 
     constructor(private readonly mailerService: MailerService) { }
 
     /**
-     * sending email after user logged in his account
-     * @param user the logged in user
+     * Sending email after user logged in to his account
+     * @param email logged in user email
      */
     public async sendLogInEmail(email: string) {
         try {
             const today = new Date();
+            
             await this.mailerService.sendMail({
                 to: email,
                 from: `<no-reply@my-nestjs-app.com>`,
                 subject: 'Log in',
-                html: `
-                 <div>
-                   <h2>Hi ${email}</h2>
-                   <p>
-                     You logged in to your account in ${today.toDateString()} at ${today.toLocaleTimeString()}
-                   </p>
-                 </div>
-                `
+                template: 'login',
+                context: { email, today }
             })
         } catch (error) {
             console.log(error);
